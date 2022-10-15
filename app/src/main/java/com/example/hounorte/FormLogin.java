597e47fde8a9b2dc956c5eae634f737be6c11e40
amonmn <84486422+amonmn@users.noreply.github.com>
2022-10-15 11:53:37 -0400

    Tela de Cadastro 15/10/2022

--- app/src/main/java/com/example/hounorte/FormLogin.java
+++ app/src/main/java/com/example/hounorte/FormLogin.java
@@ -2,13 +2,34 @@ package com.example.hounorte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView txt_cadastre_se;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+
+        IniciarComponentes();
+
+        txt_cadastre_se.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    private void IniciarComponentes(){
+        txt_cadastre_se = findViewById(R.id.txt_cadastre_se);
+    }
+
 }
