220e8c735d15348d8e29a61a24c5f8c2d5a47336
Pedro Carvalho Almeida <80465335+pedrocarvalhosnk@users.noreply.github.com>
2022-11-21 20:02:37 -0400

    Recuperação tudo que a gente perdeu nessa sprint - na proxima a gente desiste!

--- app/src/main/java/com/example/hounorte/fragments/PaginaPerfilFragment.java
+++ app/src/main/java/com/example/hounorte/fragments/PaginaPerfilFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
@@ -22,11 +23,18 @@ import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class PaginaPerfilFragment extends Fragment {
 
-    private TextView nomeUsuario, emailUsuario;
+    private TextView nomeUsuario , sobreNomeUsuario, emailUsuario, senhaUsuario, tituloNomeUsuario, tituloSobrenomeUsuario;
     private MaterialButton bt_deslogar;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,8 +43,13 @@ public class PaginaPerfilFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_pagina_perfil, container, false);
 
         nomeUsuario = view.findViewById(R.id.perfil_nome);
+        sobreNomeUsuario = view.findViewById(R.id.perfil_sobrenome);
         emailUsuario = view.findViewById(R.id.perfil_email);
+        senhaUsuario = view.findViewById(R.id.perfil_senha);
         bt_deslogar = view.findViewById(R.id.btn_logout);
+        tituloNomeUsuario = view.findViewById(R.id.tituloNomeUsuario);
+        tituloSobrenomeUsuario = view.findViewById(R.id.tituloSobreomeUsuario);
+
 
         bt_deslogar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,4 +65,27 @@ public class PaginaPerfilFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot != null){
+                    nomeUsuario.setText(documentSnapshot.getString("nome"));
+                    sobreNomeUsuario.setText(documentSnapshot.getString("sobrenome"));
+                    emailUsuario.setText(documentSnapshot.getString("email"));
+                    senhaUsuario.setText(documentSnapshot.getString("senha"));
+                    tituloNomeUsuario.setText(documentSnapshot.getString("nome"));
+                    tituloSobrenomeUsuario.setText((documentSnapshot.getString("sobrenome")));
+
+                }
+            }
+        });
+
+    }
 }
