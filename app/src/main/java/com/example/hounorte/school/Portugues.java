7f646c4cc1804ddc4de4f69aa491bfa699c37f97
amonmn <84486422+amonmn@users.noreply.github.com>
2022-11-21 22:45:27 -0400

    Atualização das fonts do projeto 21/11/2022

--- app/src/main/java/com/example/hounorte/school/Portugues.java
+++ app/src/main/java/com/example/hounorte/school/Portugues.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.hounorte.R;
+import com.google.android.material.button.MaterialButton;
 
 public class Portugues extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portugues);
     }
+
+
 }
